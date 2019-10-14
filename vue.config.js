1b3bb879e4114ea3dc7571e8834fa4d4ceadbf48
maxtcarry <maxtcarry@163.com>
2019-10-14 18:09:13 +0800

    修改pwa config配置

--- vue.config.js
+++ vue.config.js
@@ -154,28 +154,5 @@ module.exports = {
       ]
     }
   },
-  //  plugins: {
-  //   'autoprefixer': {
-  //     browsers: ['Android >= 4.0', 'iOS >= 7']
-  //   },
-  //   'postcss-pxtorem': {
-  //     rootValue: 32,//结果为：设计稿元素尺寸/16，比如元素宽320px,最终页面会换算成 20rem
-  //     propList: ['*']
-  //   }
-  // }
-  pwa: {
-    name: 'My App',
-    themeColor: '#4DBA87',
-    msTileColor: '#000000',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
 
-    // 配置 workbox 插件
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // InjectManifest 模式下 swSrc 是必填的。
-      swSrc: 'dev/sw.js',
-      // ...其它 Workbox 选项...
-    }
-  }
 }
